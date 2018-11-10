c199ab9c5bd1d9c86c7d5bdbfeb8e82e83a72107
tcshendan <664542051@qq.com>
2018-11-10 20:45:35 +0800

    项目目录升级

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const baseConfig = require('./webpack.config.base')
+
+const devServer = {
+  contentBase: path.join(__dirname, 'dist'),
+    port: 8000,
+    host: '0.0.0.0',
+    hot: true,
+    overlay: true
+}
+
+let config
+
+config = merge(baseConfig, {
+  mode: 'development',
+  devtool: '#cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.styl(us)?$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'stylus-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devServer,
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+})
+
+module.exports = config
---
+++ build/webpack.config.prod.js
@@ -0,0 +1,60 @@
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
+const baseConfig = require('./webpack.config.base')
+
+let config
+
+config = merge(baseConfig, {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: {
+    app: path.join(__dirname, '../src/index.js'),
+    vendor: ['vue']
+  },
+  output: {
+    filename: '[name].[chunkhash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.styl(us)?$/,
+        use: ExtractTextWebpackPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+            {
+                loader: 'css-loader'
+            },
+            {
+                loader: 'postcss-loader',
+                options: {
+                sourceMap: true
+                }
+            },
+            {
+                loader: 'stylus-loader'
+            }
+            ]
+        })
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new ExtractTextWebpackPlugin('styles.[hash:8].css')
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendor',
+          test: 'vendor',
+          chunks: 'all',
+          priority: 10
+        }
+      }
+    }
+  }
+})
+
+module.exports = config
